b0b7d4b996238b8446b7b8c2c6a8be119d153ca7
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2024-10-16 19:33:37 +0300

    Added game_init.rs for starting a game

---
+++ src/game_init.rs
@@ -0,0 +1,75 @@
+use std::collections::{HashMap, HashSet};
+use crate::participant;
+use crate::utils::prompt;
+
+fn init_colour_hashmap(color_map: &mut HashMap<String, String>) {
+    color_map.insert("red".to_string(), "\x1B[31m".to_string());
+    color_map.insert("blue".to_string(), "\x1B[34m".to_string());
+    color_map.insert("green".to_string(), "\x1B[32m".to_string());
+    color_map.insert("yellow".to_string(), "\x1B[33m".to_string());
+    color_map.insert("magenta".to_string(), "\x1B[35m".to_string());
+    color_map.insert("cyan".to_string(), "\x1B[36m".to_string());
+    color_map.insert("".to_string(), "".to_string());
+}
+
+pub(crate) fn read_player_count() -> Vec<participant::Participant> {
+    let mut player_count: u8 = 0;
+    while player_count <= 1 {
+        let player_count_str: String = prompt(&("How Many Players? ".to_string()));
+        player_count = player_count_str.trim().parse().unwrap_or_else(|_| {
+            0
+        });
+    }
+    let mut players: Vec<participant::Participant> = Vec::new();
+    let mut symbols: HashSet<char> = HashSet::new();
+    let mut color_name_to_ansii: HashMap<String, String> = HashMap::new();
+    init_colour_hashmap(&mut color_name_to_ansii);
+    while symbols.len() < player_count as usize {
+        let input_symbol: char = prompt(&(format!("Player {}# Symbol? ", symbols.len() + 1)))
+            .trim()
+            .parse()
+            .unwrap_or_else(|_| {
+                println!("Cannot Parse This Symbol. Please Type A Valid Character");
+                return '\0';
+            });
+        match input_symbol {
+            '\0' => { continue; }
+            ' ' => {
+                println!("Cannot input an empty symbol to use!");
+                continue;
+            }
+            _ => {}
+        }
+        if symbols.contains(&input_symbol) {
+            println!("A Player has already assigned this symbol!");
+            continue;
+        }
+        let mut input_colour: String = prompt(&(
+            format!("Player {}# Colour? ", symbols.len() + 1))
+        ).trim().parse().unwrap_or_else(|_| {
+            println!("Cannot Parse This Colour String. Please Type A Valid Colour String!");
+            return "".to_string();
+        });
+        input_colour = input_colour.trim().to_lowercase();
+        if !color_name_to_ansii.contains_key(&input_colour) {
+            println!("The Colour Does Not Exist!");
+            continue;
+        }
+        let is_bot_char: char = prompt(&(
+            format!("Player {}# Computer Or Human (Y/N)? ", symbols.len() + 1))
+        ).trim().parse().unwrap_or_else(|_| {
+            '\0'
+        });
+        if  is_bot_char.to_lowercase().to_string() != "n" && is_bot_char.to_lowercase().to_string() != "y" {
+            println!("Please Enter Yes Or No As (Y/N)!");
+            continue;
+        }
+        let is_bot: bool = is_bot_char.to_lowercase().to_string() == "y";
+        input_colour = "\x1B[1m".to_string() + &*color_name_to_ansii[&input_colour];
+        symbols.insert(input_symbol);
+        players.push(participant::Participant{
+            symbol: input_symbol, colour: input_colour, is_bot
+        });
+    }
+    players
+}
